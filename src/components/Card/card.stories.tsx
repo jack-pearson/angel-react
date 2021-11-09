39ea0de6756d501cfadecd44e7279b62efd79bfb
baba <liu@babadeMacBook-Pro.local>
2021-11-09 14:29:39 +0800

    card 组件优化

--- src/components/Card/card.stories.tsx
+++ src/components/Card/card.stories.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: jack-pearson
  * @Date: 2021-08-13 17:38:24
- * @LastEditTime: 2021-11-03 16:08:48
+ * @LastEditTime: 2021-11-09 14:14:15
  * @LastEditors: jack-pearson
  * @FilePath: /angel-ui/src/components/Card/card.stories.tsx
  * @Description: props look https://fontawesome.com/v5.15/how-to-use/on-the-web/using-with/react
@@ -9,7 +9,8 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Card, ICardProps } from './card';
-
+import Icon from '../Icon';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 export default {
   title: 'Card',
   component: Card,
@@ -19,8 +20,22 @@ const Template: ComponentStory<typeof Card> = (args: ICardProps) => <Card {...ar
 
 export const DefaultCard = Template.bind({});
 const a = <a href="http://www.baidu.com">更多</a>;
+const cover = (
+  <img alt="example" src="http://5b0988e595225.cdn.sohucs.com/images/20190118/35c70cf236a64cf6b6d6c5320da12504.jpeg" />
+);
+const actions = [
+  ['fab', '500px'],
+  ['fab', '500px'],
+  ['fab', '500px'],
+].map((item, index) => (
+  <Icon key={index} icon={item as IconProp} style={{ fontSize: '20px' }}>
+    {item}
+  </Icon>
+));
 DefaultCard.args = {
   title: 'Card title',
   children: '123',
   extra: a,
+  cover,
+  actions,
 };
